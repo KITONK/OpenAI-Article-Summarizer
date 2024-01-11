432ed03b1129ffdf4ade4aec55fd91d99cce22b3
KITONK <kiyj585@gmail.com>
2024-01-12 01:05:27 +0200

    Update GitHub link

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => (
 
       <button
         type="button"
-        onClick={() => window.open("https://github.com/KITONK/ai_summarizer")}
+        onClick={() => window.open("https://github.com/KITONK/OpenAI-Article-Summarizer")}
         className="black_btn"
       >
         GitHub
